9c592e74128b190e89828846de6886225e2a8127
Anish2504 <anish.kanswal@gmail.com>
2020-03-09 00:10:02 +0100

    test passing for user can see rps

--- cypress/integration/userCanSeeRps.feature.js
+++ cypress/integration/userCanSeeRps.feature.js
@@ -1,3 +1,5 @@
+
+
 describe("User can see three options on the page", () => {
     beforeEach(() => {
       cy.visit('http://localhost:3001');
@@ -10,3 +12,20 @@ describe("User can see three options on the page", () => {
         cy.get('#rockbutton').should('exist');
       })
     });
+  
+    it('displays second item', () => {
+      cy.get('#papercontainer').within(() => {
+        cy.get('#paperimg').should('exist');
+        cy.get('#paper').should('contain', 'Paper');
+        cy.get('#paperbutton').should('exist');
+      })
+    });
+  
+    it('displays third item', () => {
+      cy.get('#scissorscontainer').within(() => {
+        cy.get('#scissorsimg').should('exist');
+        cy.get('#scissors').should('contain', 'Scissors');
+        cy.get('#scissorbutton').should('exist');
+      })
+    });
+  });  
